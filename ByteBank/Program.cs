34eadc9a3ac0c42db0d30d4daabbb4c5dcc787f4
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov>
2022-11-15 16:40:42 -0300

    obj

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -1,1 +1,10 @@
 ﻿
+using ByteBank;
+
+ContaCorrente conta1 = new ContaCorrente();
+conta1.titular = "André Silva";
+conta1.conta = "1010-X";
+conta1.numero_agencia = 15;
+conta1.saldo = 100;
+
+Console.WriteLine(conta1.titular);
