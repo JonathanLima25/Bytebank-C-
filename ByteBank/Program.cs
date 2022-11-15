0500691b4b8ecd10760e48cea523abee73a9cf36
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov>
2022-11-15 20:46:48 -0300

    melhoria

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -12,3 +12,12 @@ Console.WriteLine("Saldo de antes do deposito: R$"+conta1.saldo);
 conta1.Depositar(100);
 
 Console.WriteLine("Saldo de depois do deposito: R$" + conta1.saldo);
+
+Console.WriteLine("--------------------------------------------");
+
+Console.WriteLine($"Titular da conta {conta1.titular}");
+Console.WriteLine($"Número da conta {conta1.conta}");
+Console.WriteLine($"Número da agencia {conta1.numero_agencia}");
+Console.WriteLine($"Saldo da conta {conta1.saldo}");
+
+Console.WriteLine("--------------------------------------------");
