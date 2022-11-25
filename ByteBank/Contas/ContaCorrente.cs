c163964ad6bbbe17d2b18c9f3d94302d9119b2ba
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov>
2022-11-25 11:16:54 -0300

    att

--- ByteBank/Contas/ContaCorrente.cs
+++ ByteBank/Contas/ContaCorrente.cs
@@ -9,7 +9,7 @@ namespace ByteBank.Contas
 {
     public class ContaCorrente
     {
-        public static int TotalDeContasCriadas { get; set; }
+        public static int TotalDeContasCriadas { get; private set; }
         public int Numero_agencia { get; set; }
         
         public string Conta { get; set; }
